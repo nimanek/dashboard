4a73259e1f910a90c4edecf0ba5b99a01aa6d5ad
Nima nek <nima.nekooyi01@gmail.com>
2026-06-13 22:24:05 +0330

    install recharts and made DailySales component dynamic

--- src/components/statCardDownData/DailySales.tsx
+++ src/components/statCardDownData/DailySales.tsx
@@ -1,9 +1,26 @@
-
+import { BarChart, Bar, ResponsiveContainer } from "recharts"
 
 export const DailySalesChart = () => {
+
+
+  const data = [
+    {value: 30},
+    {value: 45},
+    {value: 28},
+    {value: 50},
+    {value: 60},
+    {value: 35},
+    {value: 55}
+]
+
+
   return (
     <div>
-        DailySalesChart WILL BE HERE
+        <ResponsiveContainer width="100%" height={100} >
+          <BarChart data={data}>
+            <Bar dataKey="value" fill="darkcyan" radius={[4,4,0,0]}/>
+          </BarChart>
+        </ResponsiveContainer>
     </div>
   )
 }
